6092347a90af7305ff3504f49c682589e7b95095
bpmiranda3099 <brandon.miranda@lpunetwork.edu.ph>
2025-04-19 12:10:32 +0800

    Update server.js and environment variables

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const { Pool } = require('pg');
 const firebaseAdmin = require('firebase-admin');
@@ -7,7 +6,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Firebase Admin SDK
-const serviceAccount = require(process.env.FIREBASE_ADMIN_SDK);
+const serviceAccount = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure proper line breaks in the private key
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
 });
